417fc22eb029a15073f533b1f5e97d8524533da7
skoddowl <skoddowl@gmail.com>
2017-07-17 21:48:47 +0300

    ES6

--- app.js
+++ app.js
@@ -1,46 +1,59 @@
-// This loads the environment variables from the .env file
-require('dotenv-extended').load()
+const dotenv = require('dotenv-extended')
+const builder = require('botbuilder')
+const restify = require('restify')
+const tvmaze = require('tvmaze-api')
 
-var builder = require('botbuilder')
-var restify = require('restify')
-var tvmaze = require('tvmaze-api')
+// This loads the environment variables from the .env file
+dotenv.load()
 
 // Setup Restify Server
-var server = restify.createServer()
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-  console.log('%s listening to %s', server.name, server.url)
-})
+const server = restify.createServer()
+server.listen(
+  process.env.port || process.env.PORT || 3978,
+  () => {
+    console.log('%s listening to %s', server.name, server.url)
+  }
+)
 
 // Create connector and listen for messages
-var connector = new builder.ChatConnector({
+const connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 })
 server.post('/api/messages', connector.listen())
 
-var bot = new builder.UniversalBot(connector, function (session) {
-  session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text)
-})
+const bot = new builder.UniversalBot(
+  connector,
+  (session) => {
+    const message = 'Sorry, I did not understand \'%s\'. ' +
+                    'Type \'help\' if you need assistance.'
 
-var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL)
+    session.send(message, session.message.text)
+  }
+)
+
+const recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL)
 bot.recognizer(recognizer)
 
 bot.dialog('FindTVShow', [
-  function (session, args, next) {
-    var tvShow = builder.EntityRecognizer.findEntity(args.intent.entities, 'showName')
+  (session, args, _) => {
+    const tvShow =
+      builder.EntityRecognizer.findEntity(args.intent.entities, 'showName')
+
     if (tvShow) {
       session.sendTyping()
-      tvmaze.getByQuery(tvShow, true, [], function (result) {
+      tvmaze.getByQuery(tvShow, true, [], (result) => {
         session.send('I found: %s', result.id)
       })
     } else {
       builder.Prompts.text(session, 'Please enter your TV-Show name')
     }
   },
-  function (session, result) {
-    var tvShow = result.response
+  (session, result) => {
+    const tvShow = result.response
+
     session.sendTyping()
-    tvmaze.getByQuery(tvShow, true, [], function (result) {
+    tvmaze.getByQuery(tvShow, true, [], (result) => {
       session.send('I found: %s', result.id)
     })
   }
@@ -48,12 +61,15 @@ bot.dialog('FindTVShow', [
   matches: 'FindTVShow'
 })
 
-bot.dialog('Help', function (session) {
-  session.endDialog('Hi! Try asking me things like \'search tv-show\', \'tv-show schedule for today\'.')
+bot.dialog('Help', (session) => {
+  const message = 'Hi! Try asking me things like \'search tv-show\', ' +
+                  '\'tv-show schedule for today\'.'
+
+  session.endDialog(message)
 }).triggerAction({
   matches: 'Help'
 })
 
-bot.dialog('default', function (session) {
+bot.dialog('default', (session) => {
   session.endDialog('Try to search tv-show')
 })
